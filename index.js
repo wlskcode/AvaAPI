694a31029f99e1f760f0326c72b3e6af3232c957
wlskcode <caiowolski@gmail.com>
2021-07-28 00:12:20 -0300

    Added Twitter Download

--- index.js
+++ index.js
@@ -12,11 +12,18 @@ app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
 app.get('/api', async (req, res) => {
     const {url} = req.query;
     console.log(req.params);
-    const result = await InstaDl(url, ws);
-    res.status(200).send({
-        url_list: result,
-        posts: result.length
-    })
+    if(url.includes("instagram.com")){
+        const result = await InstaDl(url, ws);
+        res.status(200).send({
+            url_list: result,
+            posts: result.length
+        })
+    }else if(url.includes("twitter.com")){
+        const result = await TwitterDl(url, ws);
+        res.status(200).send({
+            url_list: result
+        })        
+    }else{res.status(403).send()}
 })
 
 puppeteer.launch(config).then(browser => {
@@ -41,6 +48,34 @@ const InstaDl = async (url, ws) => {
         data.forEach(elem => {
             const noDownload = elem.substring(0, elem.length - 5); //Removes auto download
             if (!hrefGot.includes(noDownload)) hrefGot.push(noDownload);
+        })
+        console.log(hrefGot);
+        page.close();
+        return hrefGot;
+    }catch (err){
+        console.log(err);
+    }
+}
+
+const TwitterDl = async (url, ws) => {
+    try{
+    const browser = await puppeteer.connect({browserWSEndpoint: ws}).catch(err => console.log(err))
+
+        const page = await browser.newPage();
+        await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148')
+        const downloadUrl = url.replace("twitter", "ssstwitter");
+        console.log(downloadUrl)
+        await page.goto(downloadUrl, { waitUntil: 'networkidle0' });
+
+        const data = await page.evaluate(() => Array.from(document.querySelectorAll('a[download]'), e => e.href));
+        let hrefGot = [];
+        let maxRes = 0;
+        data.forEach(elem => {
+            let resolution = Number((elem.split('/')[7]).slice(4))
+            console.log(resolution)
+            if (resolution > maxRes) {
+                hrefGot = elem ;
+                maxRes = resolution;}
         })
         console.log(hrefGot);
         page.close();
